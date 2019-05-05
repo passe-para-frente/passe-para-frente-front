aca2a6648914855a672cecd3d43f21e727054cd9
yagosenhorini <yagosenhorini30@gmail.com>
2019-05-05 01:17:28 -0300

    troca de importacao

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/header/header';
 import './app.scss';
 import SectionHome from './components/section-home/section';
-import { About } from './components/about/about';
+import About from './components/about/about';
 
 class App extends React.Component{
     render(){
