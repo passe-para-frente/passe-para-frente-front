44bb6645f0035349762325d8c07b7ddf3e248c06
yagosenhorini <yagosenhorini30@gmail.com>
2019-05-26 13:38:16 -0300

    [DESK/MOB] Ajuste de CSS para responsivo e criaçao de funcao para abrir menu em dispositivos moveis | [CSS/JS]

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className="pf-header">
             <nav className="pf-menu">
                 <a href="/">
                     Passe para Frente
                 </a>
+                <div className="pf__menu-mobile" onClick={props.openMenu}>
+                    <div className="pf__menu-mobile-bar"></div>
+                    <div className="pf__menu-mobile-bar"></div>
+                    <div className="pf__menu-mobile-bar"></div>
+                </div>
                 <ul className="pf-profile">
                     <li><a href="#">Sobre</a></li>
                     <li><a href="#">Contato</a></li>
